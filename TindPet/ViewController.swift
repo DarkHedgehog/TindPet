6bd077e7eb12e57d81a94fc331a00c72953b7b05
Derevenskikh Aleksander <hedgehog@kartis.ru>
2023-06-21 10:56:07 +0300

    Initial Commit

---
+++ TindPet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TindPet
+//
+//  Created by Aleksandr Derevenskih on 21.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
