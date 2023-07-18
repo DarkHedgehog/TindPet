9a38b8bfd2aff2907972709157a1e488bced1ac1
Lyoshka <alex.sidorenko@mail.ru>
2023-07-18 11:45:25 +0300

    add swipe screen

---
+++ TindPet/App/Flows/Swipes/View/SwipesViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SwipesViewController.swift
+//  TindPet
+//
+//  Created by Aleksandr Derevenskih on 21.06.2023.
+//
+
+import UIKit
+
+protocol SwipesPresenterProtocol {
+    func likeButtonAction()
+    func dislikeButtonAction()
+}
+
+class SwipesViewController: UIViewController {
+    var presenter: SwipesPresenterProtocol?
+
+    private var swipeView: SwipesView {
+        return self.view as! SwipesView
+    }
+
+    
+    //MARK: - Init
+    override func loadView() {
+        super.loadView()
+        self.view = SwipesView()
+    }
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension SwipesViewController: SwipeCardsDelegate {
+    func likeButtonAction() {
+        print("Like")
+    }
+    
+    func dislikeButtonAction() {
+        print("Dislike")
+    }
+}
+
+extension SwipesViewController: SwipesViewProtocol {
+}
