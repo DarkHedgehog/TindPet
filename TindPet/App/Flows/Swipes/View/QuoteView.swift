5cb47fedf592ff2fd7a7550bb77dfc230593176b
Derevenskikh Aleksander <hedgehog@kartis.ru>
2023-08-10 13:28:40 +0300

    fix swipes

--- TindPet/App/Flows/Swipes/View/QuoteView.swift
+++ TindPet/App/Flows/Swipes/View/QuoteView.swift
@@ -17,7 +17,6 @@ class QuoteView: UIView {
         label.isUserInteractionEnabled = true
         label.translatesAutoresizingMaskIntoConstraints = false
         label.backgroundColor = UIColor.systemGray6
-//        label.backgroundColor = .clear
         label.textAlignment = .justified
         label.layer.cornerRadius = 10
         label.layer.masksToBounds = true
@@ -40,7 +39,6 @@ class QuoteView: UIView {
         imageView.isUserInteractionEnabled = false
         imageView.alpha = 1
         imageView.layer.cornerRadius = 20
-//        imageView.image = UIImage(systemName: "hand.thumbsup.fill")
         imageView.tintColor = UIColor.green
         imageView.layer.borderWidth = 1
         return imageView
@@ -60,26 +58,21 @@ class QuoteView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         if animator == nil {
-//            label.frame = bounds.insetBy(dx: 20, dy: 10)
             thumbImageView.frame = bounds.insetBy(dx: 20, dy: 10)
             petImageView.frame = bounds.insetBy(dx: 20, dy: 10)
         }
     }
     private func setup() {
         addSubview(petImageView)
-        addSubview(label)
+        petImageView.addSubview(label)
         addSubview(thumbImageView)
 
         NSLayoutConstraint.activate([
             label.bottomAnchor.constraint(equalTo: petImageView.bottomAnchor, constant: -10),
             label.leftAnchor.constraint(equalTo: petImageView.leftAnchor, constant: 10),
             label.rightAnchor.constraint(equalTo: petImageView.rightAnchor, constant: -10)
-//            nameLabel.rightAnchor.constraint(equalTo: rightAnchor)
         ])
 
-//        label.layer.borderWidth = 1
-//        label.layer.cornerRadius = 10
-       
         let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan(_:)))
         self.addGestureRecognizer(panRecognizer)
     }
@@ -90,7 +83,6 @@ extension QuoteView {
     @objc private func swipe(sender: UISwipeGestureRecognizer) {
         switch sender.direction {
         case .left:
-            label.frame.origin.x -= 100
             thumbImageView.frame.origin.x -= 100
             thumbImageView.tintColor = UIColor.systemRed
             thumbImageView.image = UIImage(systemName: "hand.thumbsdown.fill")
@@ -100,7 +92,6 @@ extension QuoteView {
             petImageView.alpha = 0
 
         case .right:
-            label.frame.origin.x += 100
             thumbImageView.frame.origin.x += 100
             thumbImageView.tintColor = UIColor.systemGreen
             thumbImageView.image = UIImage(systemName: "hand.thumbsup.fill")
@@ -112,7 +103,6 @@ extension QuoteView {
             break
         }
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.label.frame = self.bounds
             self.thumbImageView.frame = self.bounds
             self.thumbImageView.image = nil
             self.thumbImageView.alpha = 0
@@ -126,40 +116,43 @@ extension QuoteView {
         case .began:
             like = translation > 0
 
-            let endX = like ? 3 * bounds.width / 2 + 50 : -bounds.width / 2 - 50
+            let endX = like ? 3 * bounds.width / 2 + 50: -bounds.width / 2 - 50
             let angle = like ? CGFloat.pi / 4 : -CGFloat.pi / 4
             let thumbImage = like ? "hand.thumbsup.fill" : "hand.thumbsdown.fill"
             thumbImageView.image = UIImage(systemName: thumbImage)
             thumbImageView.tintColor = like ? UIColor.green : UIColor.red
 
             animator = UIViewPropertyAnimator(duration: 0.5, curve: .easeIn) {
-                [ self.thumbImageView, self.petImageView].forEach {
+                [self.thumbImageView, self.petImageView].forEach {
                     $0.center = CGPoint(x: endX, y: self.bounds.height / 2)
                     $0.transform = CGAffineTransform(rotationAngle: angle)
                 }
                 self.thumbImageView.alpha = 1
                 self.petImageView.alpha = 0
             }
-            animator?.addCompletion { [weak self] some in
-                guard let self = self else { return }
 
-                [self.label, self.thumbImageView, self.petImageView].forEach {
+            animator?.addCompletion { [weak self] _ in
+                guard let self = self, let animator = animator else { return }
+
+                [self.thumbImageView, self.petImageView].forEach {
                     $0.center = CGPoint(x: self.bounds.width / 2, y: self.bounds.height / 2)
                     $0.transform = .identity
                 }
                 self.thumbImageView.alpha = 0
                 self.petImageView.alpha = 1
 
-                self.alpha = 0
                 self.didRate?()
 
-                UIView.animate(withDuration: 0.5) {
-                    self.alpha = 1
-                }
-                if like {
-                    self.presenter?.likeButtonAction()
-                } else {
-                    self.presenter?.dislikeButtonAction()
+                if !animator.isReversed {
+                    self.alpha = 0
+                    UIView.animate(withDuration: 0.5) {
+                        self.alpha = 1
+                    }
+                    if like {
+                        self.presenter?.likeButtonAction()
+                    } else {
+                        self.presenter?.dislikeButtonAction()
+                    }
                 }
             }
 
@@ -167,7 +160,14 @@ extension QuoteView {
             let slide = like ? max(translation, 0) : min(translation, 0)
             animator?.fractionComplete = abs(slide) / bounds.width
         case .ended:
-            animator?.startAnimation()
+            let edge = 100.0
+            if translation >= edge || translation <= -edge {
+                animator?.startAnimation()
+            } else {
+                animator?.isReversed = true
+                animator?.startAnimation()
+            }
+
         default:
             break
         }
